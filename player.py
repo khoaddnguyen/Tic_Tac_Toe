94134f7e410c6a50b2746f51a51ec6a0bd239585
kwa_laa <khoaddnguyen@gmail.com>
2023-10-10 15:25:27 -0500

    initial commit

---
+++ player.py
@@ -0,0 +1,46 @@
+import math
+import random
+
+
+class Player:
+
+    def __init__(self, letter):
+        # letter is X or O
+        self.letter = letter
+
+    # all players get their next move
+    def ger_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        # get a random valid spot
+        square = random.choice(game.available_moves())
+        return square
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "\'s turn. Input move (0-8): ")
+            # invalid if inout is not an integer or spot is not available on board
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again.")
+
+        return val
+
+
